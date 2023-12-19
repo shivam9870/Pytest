33591f504923c961ab0936bb0c388117db25fae5
Shivam9870 <ssv2074@gmail.com>
2023-12-19 12:55:43 +0530

    04 dec 2023

--- venv/30oct2023/lab049.py
+++ venv/30oct2023/lab049.py
@@ -1,17 +1,17 @@
-#Dictionary in Python
+# Dictionary in Python
 # Key and Value Pair.
 # A dictionary stores key-value pairs, where each unique key is an index which holds the value associated with it.
 # Dictionaries are unordered because the entries are not stored in a linear structure.
 
-name="Shivam"
-#Key -> name
-#value -> Shivam
+name = "Shivam"
+# Key -> name
+# value -> Shivam
 
-#A dictionary is an unordered collection of data in a key value pair format.
-#It is defined using curly braces {} and consists of key and their associated values.
+# A dictionary is an unordered collection of data in a key value pair format.
+# It is defined using curly braces {} and consists of key and their associated values.
 
-my_dict={}
-my_dict2=dict()
+my_dict = {}
+my_dict2 = dict()
 
 print(type(my_dict))
 print(type(my_dict2))
